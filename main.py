ea7a7882faa70f8119231e182f85fb3d0af3573a
Abhinav Dobhal <abhinav.dobhal@gmail.com>
2022-09-02 17:13:27 +0530

    fix get error

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 
 app = FastAPI()
+app = FastAPI(title="EmembrApp1", description="EmembrApp for financial transaction", version="1.0.0",
+              openapi_url="/api/openapi.json", docs_url="/api/docs/", redoc_url=None)
 
 
-@get.get('/')
-async def root():
+@app.get("/")
+def main():
     return {'message': 'Demo for fastapi github action for heroku!'}
